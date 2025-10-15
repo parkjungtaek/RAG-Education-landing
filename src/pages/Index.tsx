4403f5fd647c0e46dc06eae5e8326dd8d86da70e
parkjungtaek <ypjt7409@gmail.com>
2025-10-15 19:59:26 +0900

    Updated Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,7 +6,6 @@ import { TechStackSection } from "@/components/TechStackSection";
 import { BenefitsSection } from "@/components/BenefitsSection";
 import { CurriculumSection } from "@/components/CurriculumSection";
 import { ProjectsSection } from "@/components/ProjectsSection";
-import { CompaniesSection } from "@/components/CompaniesSection";
 import { CTAFooter } from "@/components/CTAFooter";
 import { ConsultationModal } from "@/components/ConsultationModal";
 
@@ -22,7 +21,6 @@ const Index = () => {
       <BenefitsSection />
       <CurriculumSection />
       <ProjectsSection />
-      <CompaniesSection />
       <CTAFooter onOpenModal={() => setIsModalOpen(true)} />
       <ConsultationModal open={isModalOpen} onOpenChange={setIsModalOpen} />
     </div>
