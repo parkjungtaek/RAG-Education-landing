bfb901d673a019f7da44c4c81df99d9e329e5555
parkjungtaek <ypjt7409@gmail.com>
2025-10-17 15:03:35 +0900

    Updated OverviewSection.tsx

--- src/components/OverviewSection.tsx
+++ src/components/OverviewSection.tsx
@@ -198,7 +198,7 @@ export const OverviewSection = () => {
           </p>
         </motion.div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {cards.map((card, index) => (
             <motion.div
               key={index}
